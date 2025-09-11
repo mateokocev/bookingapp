3e545c28a2718bf55ea4b9035f17ae40c5b9f7e6
Andrej Korica <60071194+andrejkorica@users.noreply.github.com>
2025-09-11 21:57:24 +0200

    fixed

--- FE/nuxt.config.ts
+++ FE/nuxt.config.ts
@@ -2,8 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  "ui": {
-    "fonts": false
-  }
+  modules: ["@nuxt/image", "@nuxt/ui"],
+  css: ['~/assets/css/main.css']
 })
---
+++ FE/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <UButton color="neutral">Button</UButton>
+
+</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
