82b87ee07f67c0452fc8e2653d3294cdf17d0944
Andrej Korica <60071194+andrejkorica@users.noreply.github.com>
2025-09-18 11:37:53 +0200

    [UPDATE] fix
    config adapted

--- FE/nuxt.config.ts
+++ FE/nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ["@nuxt/image", "@nuxt/ui", "@pinia/nuxt"],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:8080/api'
+    }
+  }
 })
